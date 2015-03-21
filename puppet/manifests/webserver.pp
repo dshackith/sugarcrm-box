a212c682281458d9121036cab9de7536dba7fa40
Daniel Shackelford <daniel.shackelford@ithaka.org>
2015-03-20 23:59:59 -0400

    Update webserver.pp

--- puppet/manifests/webserver.pp
+++ puppet/manifests/webserver.pp
@@ -23,7 +23,7 @@ class webserver {
     docroot       => '/var/www/html',
     override      => 'All',
   }
-  #php::module { [ 'mysql', 'ldap', 'pdo','mbstring' ]: }
+  php::module { [ 'mysql', 'ldap', 'pdo','mbstring' ]: }
   
   php::ini { '/etc/php5/conf.d/memory_limit.ini':
   memory_limit   => '512M'
