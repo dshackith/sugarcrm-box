4abe4918e5de5c12cbf0156cf96c07711f414664
Daniel Shackelford <daniel.shackelford@ithaka.org>
2015-03-20 23:17:25 -0400

    Update db.pp

--- puppet/manifests/db.pp
+++ puppet/manifests/db.pp
@@ -11,11 +11,6 @@ class db {
     password => 'sugarcrm',
     host     => 'localhost',
     grant    => ['all'],
-    sql      => ['/tmp/share/sugar_db_structure.sql']
-  }
-  exec { 'import mysql':
-    path  => '/usr/bin:/usr/sbin',
-    command => 'mysql -h localhost -u sugarcrm -psugarcrm sugarcrm < /tmp/share/sugar_basic_data.sql',
-    require => mysql::db['sugarcrm']
+    sql      => ['/tmp/share/skinny_db.sql']
   }
 }
