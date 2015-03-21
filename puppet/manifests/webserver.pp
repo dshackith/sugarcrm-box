c0a3ec78860742f7abde0f9d82ca7ac220c2d119
Daniel Shackelford <daniel.shackelford@ithaka.org>
2015-03-21 08:39:53 -0400

    Update webserver.pp

--- puppet/manifests/webserver.pp
+++ puppet/manifests/webserver.pp
@@ -3,7 +3,7 @@ define apache::loadmodule () {
      exec { "/usr/sbin/a2enmod $name" :
           unless => "/bin/readlink -e /etc/apache2/mods-enabled/${name}.load",
           notify => Service[apache2],
-          requires => Package[apache2]
+          require => Package[apache2]
      }
 }
 
