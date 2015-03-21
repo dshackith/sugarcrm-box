7cdddc1a55c556ac6a3d7e8147683702bd740e06
Daniel Shackelford <daniel.shackelford@ithaka.org>
2015-03-21 08:27:34 -0400

    Update webserver.pp

--- puppet/manifests/webserver.pp
+++ puppet/manifests/webserver.pp
@@ -9,7 +9,7 @@ define apache::loadmodule () {
 class webserver {
   import 'apache'
   class {'apache': }
-  package {['php5', 'php5-mysql', 'php5-gd', 'php5-imap', 'php-apc', 'php5-memcached',  'unzip', 'php5-curl']: }  
+  package {['php5', 'php5-mysql', 'php5-gd', 'php5-imap', 'php-apc', 'php5-memcached',  'unzip', 'php5-curl', 'php5-mbstring']: }  
   # class { 'mysql::server': }
   # class { 'mysql': }
 
@@ -23,7 +23,7 @@ class webserver {
     docroot       => '/var/www/html',
     override      => 'All',
   }
-  php::module { [ 'mysql', 'ldap', 'pdo','mbstring' ]: }
+  #php::module { [ 'mysql', 'ldap', 'pdo','mbstring' ]: }
   
   php::ini { '/etc/php5/conf.d/memory_limit.ini':
   memory_limit   => '512M'
